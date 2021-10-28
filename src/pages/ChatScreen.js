c57b545e7a62c0514d7b2586809357c89abd53f2
mata <mata@matas-MacBook-Air.local>
2021-10-28 12:48:22 +0200

    pull messages from firebase and show them in the app ui

--- src/pages/ChatScreen.js
+++ src/pages/ChatScreen.js
@@ -1,13 +1,15 @@
 import { useState, useEffect, useRef } from 'react'
 import { AddCircle, CreditCard, Gif, EmojiEmotions } from '@material-ui/icons'
 import ChatHeader from '../components/ChatHeader'
+import Message from '../components/Message'
 
 import firebaseApp from '../firebase/credentials'
-import { getFirestore, doc, setDoc } from 'firebase/firestore'
+import { getFirestore, doc, setDoc, collection, getDocs } from 'firebase/firestore'
 const firestore = getFirestore(firebaseApp)
 
 function ChatScreen({ currentChannel, user }) {
     const [inputMessage, setInputMessage] = useState('')
+    const [messageList, setMessageList] = useState([])
 
     function sendMessage(e) {
         e.preventDefault()
@@ -22,7 +24,25 @@ function ChatScreen({ currentChannel, user }) {
         })
 
         setInputMessage("")
+        getMessageList()
     }
+
+    async function getMessageList() {
+        const messagesArr = []
+
+        const collectionRef = collection(firestore, `channels/${currentChannel}/messages`)
+
+        const messagesC = await getDocs(collectionRef)
+        messagesC.forEach(message => {
+            messagesArr.push(message.data())
+        })
+
+        setMessageList([...messagesArr])
+    }
+
+    useEffect(() => {
+        getMessageList()
+    }, [currentChannel])
 
     return (
         <div className="chat">
@@ -30,6 +50,11 @@ function ChatScreen({ currentChannel, user }) {
 
             <div className="chat__messages">
                 {/* MAP messages from firestore */}
+                {   messageList ? messageList.map(message => {
+                    return <Message firebaseMessage={message}/>
+                }) : null
+
+                }
             </div>
             <div className="chat__input">
                 <AddCircle fontSize="large" />
